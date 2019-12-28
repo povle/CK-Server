95d35f3ae3c00a37fe6a3527ea89de336bb22637
povle <pasha@blinov.co>
2019-12-28 12:31:36 +0300

    make handler an abc

--- lib/handler.py
+++ lib/handler.py
@@ -1,15 +1,18 @@
 from command import Command
 from types import Type
+from abc import ABC, abstractmethod
 
-class Handler:
+class Handler(ABC):
     def __init__(self, arg_types={}, answer_types={}):
         self.arg_types = arg_types
         self.answer_types = answer_types + {Type.ERROR, Type.OK}
 
+    @abstractmethod
     def parse(self, raw: dict) -> Command:
         '''Parse raw input'''
         pass
 
+    @abstractmethod
     def handle(self, command: Command):
         '''Aggregate all of the responses to the command and send them back if necessary'''
         pass
